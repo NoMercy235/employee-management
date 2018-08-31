f518b12f785b0ab4c7b9e879cb8354c8addea59f
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2018-08-31 21:48:48 +0300

    Implemented export/import functionality.

--- src/app/actions/actions.component.ts
+++ src/app/actions/actions.component.ts
@@ -3,6 +3,8 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalsService } from '../services/globals.service';
 import { LocalStorageService } from 'angular-web-storage';
 import { EmployeeTemplateComponent } from '../templates/employee.template';
+import { saveAs } from 'file-saver/FileSaver';
+import { EmployeeModel } from '../templates/employee.model';
 
 @Component({
   selector: 'em-actions',
@@ -20,7 +22,7 @@ export class ActionsComponent implements OnInit {
   ngOnInit() {
   }
 
-  public openModal() {
+  public openModal(): void {
     this.modalService
       .open(EmployeeTemplateComponent, { size: 'lg' })
       .result
@@ -33,4 +35,25 @@ export class ActionsComponent implements OnInit {
       );
   }
 
+  public exportData(): void {
+    const fileContent = JSON.stringify(
+      this.localStorage.get('employees'),
+    );
+    const blob = new Blob(
+      [fileContent],
+      { type: 'application/json' }
+      );
+    saveAs(blob, 'angajati.txt');
+  }
+
+  public importData(files: any): void {
+    const fileReader = new FileReader();
+    fileReader.onload = (ev) => {
+      const employees = JSON.parse(<string>fileReader.result)
+        .map(e => new EmployeeModel(e));
+      this.globalsService.employees = employees;
+      this.localStorage.set('employees', employees);
+    };
+    fileReader.readAsText(files[0]);
+  }
 }
